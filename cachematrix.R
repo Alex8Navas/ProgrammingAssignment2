20586ee7b9c87882fbc18ba2502764178a7fc836
Kattaniss <69650894+Alex8Navas@users.noreply.github.com>
2020-11-11 14:13:12 +0100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,81 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Put comments here that give an overall description of what your functions do
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# Functions to save the inverse of an array.
+# Assumption: all arrays supplied to the function must be inverse. 
 
+
+# Crear un objeto especial con estructura de matriz que pueda guardar la inversa. 
+makeCacheMatrix <- function(m = matrix()){
+  
+  # Initialize the inverse.
+  i <- NULL
+  
+  # Method for setting the matrix. 
+  set <- function(matrix){
+    
+    m <<- matrix
+    
+    i <<- NULL
+    
+  }
+  
+  # Method to get the matrix. 
+  get <- function(){
+    
+    m
+    
+  }
+  
+  # Method for calculating the inverse of the matrix. 
+  setInverse <- function(inverse){
+    
+    i <<- inverse
+    
+  }
+  
+  # Method to fetch the inverse of the matrix. 
+  getInverse <- function(){
+    
+    i
+    
+  }
+  
+  # A list that will return the results of each of the functions.
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
+# Write a short comment describing this function
 
-## Write a short comment describing this function
-
+# Function to calculate the inverse of the array created with makeCacheMatrix.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getInverse()
+  
+  
+  if( !is.null(m) ) {
+    
+    message("getting cached data")
+    
+    return(m)
+    
+  } else {
+    
+    # Get the matrix of the created object.
+    data <- x$get()
+    
+    # Calculate the inverse of the matrix. 
+    m <- solve(data) %*% data
+    
+    # Set the inverse in the object's memory.
+    x$setInverse(m)
+    
+    # Return a matrix that is the inverse of x.
+    m
+    
+  }
+  
 }
